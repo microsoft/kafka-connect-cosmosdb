b5bff0b1eea245af916a1b20f1a414e88ff8209f
Jyotsna Ravikumar <jyraviku@microsoft.com>
2019-06-19 13:06:34 -0700

    adding more test

--- src/test/scala/com/microsoft/azure/cosmosdb/kafka/connect/sink/CosmosDBSinkTaskText.scala
+++ src/test/scala/com/microsoft/azure/cosmosdb/kafka/connect/sink/CosmosDBSinkTaskText.scala
@@ -83,5 +83,64 @@ class CosmosDBSinkTaskText extends FlatSpec with GivenWhenThen with LazyLogging 
 
   }
 
+  "CosmosDBSinkTask put" should "throw an assert error for the collection that does not exist" in {
+    Given("A Kafka Broker with an Embedded Connect and a CosmosSinkConnector instance")
+    // Start cluster and Connect
+    kafkaCluster = new KafkaCluster()
+    val workerProperties: Properties = TestConfigurations.getWorkerProperties(kafkaCluster.BrokersList.toString)
+    val props: Properties = TestConfigurations.getSourceConnectorProperties()
+    kafkaCluster.startEmbeddedConnect(workerProperties, List(props))
+
+
+    When(s"Insert ${NUM_DOCS} documents in the test collection")
+
+    val json = scala.io.Source.fromFile(getClass.getResource(s"/test1.json").toURI.getPath).mkString
+
+    val records = for (i <- 1 to 4) yield {
+      val json = scala.io.Source.fromFile(getClass.getResource(s"/test$i.json").toURI.getPath).mkString
+      new SinkRecord("cosmosdb-source-topic", 0, null, null, Schema.STRING_SCHEMA, json, i)
+    }
+
+    // Start CosmosDBSinkConnector and return the taskConfigs
+    val connector = new CosmosDBSinkConnector
+    connector.start(Maps.fromProperties(props))
+    val taskConfigs = connector.taskConfigs(2)
+
+    taskConfigs.forEach(config => {
+      When("CosmosSinkTask is started and put is called")
+      val task = new CosmosDBSinkTask
+      task.start(config)
+
+      task.put(scala.collection.JavaConversions.seqAsJavaList(records))
+    })
+
+    val clientSettings = CosmosDBClientSettings(
+      TestConfigurations.ENDPOINT,
+      TestConfigurations.MASTER_KEY,
+      TestConfigurations.DATABASE,
+      "notexistingcollection",
+      true,
+      true,
+      ConnectionPolicy.GetDefault(),
+      ConsistencyLevel.Session
+    )
+    val client = Try(CosmosDBProvider.getClient(clientSettings)) match {
+      case Success(conn) =>
+        logger.info("Connection to CosmosDB established.")
+        conn
+      case Failure(f) => throw new ConnectException(s"Couldn't connect to CosmosDB.", f)
+    }
+
+    When("Call CosmosDB queryCollection")
+
+    val docCollQry = CosmosDBProvider.queryCollection(TestConfigurations.DATABASE, TestConfigurations.COLLECTION, new CountDownLatch(1)).toBlocking.single
+    logger.info("size" + docCollQry.getResults.size.toString)
+
+
+    Then("Verify records inserted into Cosmos ")
+    assert(docCollQry.getResults.size != 0)
+
+  }
+
 
 }
