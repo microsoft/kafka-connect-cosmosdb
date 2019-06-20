a8db1a7138ce0d2c628be463bece88268da84175
Davide Mauri <damauri@microsoft.com>
2019-06-20 11:23:29 -0700

    added support for include and exclude selector

--- src/main/scala/com/microsoft/azure/cosmosdb/kafka/connect/processor/source/SelectorSourcePostProcessor.scala
+++ src/main/scala/com/microsoft/azure/cosmosdb/kafka/connect/processor/source/SelectorSourcePostProcessor.scala
@@ -18,15 +18,35 @@ class SelectorSourcePostProcessor extends JsonPostProcessor {
 
   var selectorFields = Seq.empty[String]
   var selectorType: SelectorType = SelectorType.Include
+  var processor: JsonObject => JsonObject = includeFields
 
   override def configure(config: CosmosDBConfig): Unit = {
 
     selectorFields = config.getString("connect.cosmosdb.source.post-processor.selector.fields").split(',').toSeq
     selectorType = SelectorType.fromString(config.getString("connect.cosmosdb.source.post-processor.selector.type"))
 
+    processor = selectorType match {
+      case Include => includeFields
+      case Exclude => excludeFields
+    }
+
   }
 
-  override def runJsonPostProcess(json: JsonObject): JsonObject = {
+  override def runJsonPostProcess(json: JsonObject): JsonObject = processor(json)
+
+  private def includeFields(json: JsonObject): JsonObject = {
+
+    val toInclude = selectorFields
+
+    val newJson: JsonObject = new JsonObject()
+
+    toInclude.foreach(e => newJson.add(e.trim, json.get(e.trim)))
+
+    newJson
+
+  }
+
+  private def excludeFields(json: JsonObject): JsonObject = {
 
     val toRemove = selectorFields
 
