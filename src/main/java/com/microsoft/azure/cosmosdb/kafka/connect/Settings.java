3a01b9ac9814888fcf601dfc8fd615299e75c87a
Yev Bronshteyn <yebronsh@microsoft.com>
2020-04-23 22:18:14 -0400

    Adding Topic Collection map and cosmos destination to settings.

--- src/main/java/com/microsoft/azure/cosmosdb/kafka/connect/Settings.java
+++ src/main/java/com/microsoft/azure/cosmosdb/kafka/connect/Settings.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Settings {
-    private static Logger logger = LoggerFactory.getLogger(Settings.class);
+    private static final Logger logger = LoggerFactory.getLogger(Settings.class);
 
     public static final String PREFIX = "connect.cosmosdb";
 
@@ -17,7 +17,11 @@ public class Settings {
             //Add all settings here:
             new NumericSetting(PREFIX + ".task.pollinginterval", 1000L, this::setPollingInterval, this::getPollingInterval),
             new NumericSetting(PREFIX + ".task.timeout", 5000L, this::setTaskTimeout, this::getTaskTimeout),
-            new NumericSetting(PREFIX + ".task.buffer.size", 10000L, this::setTaskBufferSize, this::getTaskBufferSize)
+            new NumericSetting(PREFIX + ".task.buffer.size", 10000L, this::setTaskBufferSize, this::getTaskBufferSize),
+            new Setting(PREFIX+".cosmosdb.endpoint", this::setEndpoint, this::getEndpoint),
+            new Setting(PREFIX+".cosmosdb.key", this::setKey, this::getKey),
+            new Setting(PREFIX+".cosmosdb.databasename", this::setDatabaseName, this::getDatabaseName),
+            new Setting(PREFIX+".collections.topicmap", value -> TopicCollectionMap.deserialize(value), ()->this.getTopicCollectionMap().serialize())
     );
 
     /**
@@ -102,6 +106,97 @@ public class Settings {
      */
     public void setTaskBufferSize(Long taskBufferSize) {
         this.taskBufferSize = taskBufferSize;
+    }
+
+
+    private String endpoint;
+
+    /**
+     * Returns the CosmosDB Endpoint
+     * @return The CosmosDB Endpoint
+     */
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    /**
+     * Sets the cosmosDB endpoint
+     * @param endpoint The cosmosDB endpoint
+     */
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    private String key;
+
+    /**
+     * Returns the CosmosDB Key
+     * @return The CosmosDB Key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the CosmosDB key
+     * @param key The CosmosDB key
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    private String databaseName;
+
+    /**
+     * Retrieves the CosmosDB database name
+     * @return
+     */
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    /**
+     * Sets the CosmosDB Database Name
+     * @param databaseName The CosmosDB Database Name
+     */
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    private String containerName;
+
+    /**
+     * Retrieves the CosmosDB container name
+     * @return The CosmosDB container name
+     */
+    public String getContainerName() {
+        return containerName;
+    }
+
+    /**
+     * Sets the CosmosDB container name
+     * @param containerName The CosmosDB container name
+     */
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+
+    private TopicCollectionMap topicCollectionMap;
+
+    /**
+     * Gets the map of Kafka topics to CosmosDB collections
+     * @return
+     */
+    public TopicCollectionMap getTopicCollectionMap() {
+        return topicCollectionMap;
+    }
+
+    /**
+     * Sets the map of Kafka topics to CosmosDB collections
+     * @param topicCollectionMap
+     */
+    public void setTopicCollectionMap(TopicCollectionMap topicCollectionMap) {
+        this.topicCollectionMap = topicCollectionMap;
     }
 }
 
