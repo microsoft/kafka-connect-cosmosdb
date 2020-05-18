5f35bd2db30a35572d44436e7613f46d9815ae97
Yev Bronshteyn <yebronsh@microsoft.com>
2020-05-18 18:48:34 -0400

    Integration test now checks Cosmos, reads configuration from connector config.

--- src/integration-test/java/com/microsoft/azure/cosmosdb/kafka/connect/sink/PostToKafkaTest.java
+++ src/integration-test/java/com/microsoft/azure/cosmosdb/kafka/connect/sink/PostToKafkaTest.java
@@ -1,11 +1,18 @@
 package com.microsoft.azure.cosmosdb.kafka.connect.sink;
 
 
+import com.azure.cosmos.CosmosClient;
+import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.CosmosContainer;
+import com.azure.cosmos.CosmosDatabase;
 import com.azure.cosmos.implementation.BadRequestException;
+import com.azure.cosmos.models.FeedOptions;
+import com.azure.cosmos.util.CosmosPagedIterable;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.cosmosdb.kafka.connect.IntegrationTest;
 import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,11 +20,16 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.connect.json.JsonSerializer;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -30,16 +42,34 @@ import static org.junit.Assert.assertNotNull;
  */
 @Category(IntegrationTest.class)
 public class PostToKafkaTest {
-
+    private static String databaseName;
+    private static String topic;
     private static Logger logger = LoggerFactory.getLogger(PostToKafkaTest.class);
-    private String topic;
+    private static CosmosContainer targetContainer;
     private Properties kafkaProperties;
+
+    @BeforeClass
+    public static void readConnectionData() throws URISyntaxException, IOException {
+        URL configFileUrl = PostToKafkaTest.class.getClassLoader().getResource("sink.config.json");
+        JsonNode config = new ObjectMapper().readTree(configFileUrl).get("config");
+        String topicContainerMap = config.get("connect.cosmosdb.containers.topicmap").textValue();
+        topic = StringUtils.substringBefore(topicContainerMap, "#");
+
+        //Get the Cosmos container for reading messages.
+        databaseName = config.get("connect.cosmosdb.cosmosdb.databasename").textValue();
+
+        CosmosClient cosmosClient = new CosmosClientBuilder()
+                .endpoint(config.get("connect.cosmosdb.connection.endpoint").textValue())
+                .key(config.get("connect.cosmosdb.master.key").textValue())
+                .buildClient();
+
+        CosmosDatabase targetDatabase = cosmosClient.getDatabase(databaseName);
+        targetContainer = targetDatabase.getContainer(StringUtils.substringAfter(topicContainerMap, "#"));
+    }
 
     @Before
     public void setUp() {
-        topic = "connect-test";
         assertNotNull("kafka_topic variable must be set.");
-
         kafkaProperties = new Properties();
         kafkaProperties.put("bootstrap.servers", "localhost:9092");
         kafkaProperties.put("client.id", "IntegrationTest");
@@ -57,6 +87,11 @@ public class PostToKafkaTest {
         try (KafkaProducer<String, JsonNode> producer = new KafkaProducer<>(kafkaProperties)) {
             producer.send(personRecord).get();
         }
+        FeedOptions options = new FeedOptions();
+        CosmosPagedIterable<Person> readResponse = targetContainer.readAllItems(new FeedOptions(), Person.class);
+        Optional<Person> retrievedPerson = readResponse.stream().filter(item -> item.getId().equals(person.getId())).findFirst();
+        Assert.assertNotNull("Person could not be retrieved", retrievedPerson.orElse(null));
+
     }
 
     @Test
@@ -75,6 +110,9 @@ public class PostToKafkaTest {
         String name;
         String id;
 
+        public Person() {
+        }
+
         public Person(String name, String id) {
             this.name = name;
             this.id = id;
@@ -86,6 +124,14 @@ public class PostToKafkaTest {
 
         public String getId() {
             return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 
