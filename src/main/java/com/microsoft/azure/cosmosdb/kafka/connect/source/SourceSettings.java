b4f75d225861447a881a0be6bbae41931cf9ddda
sivamu <67930841+sivamu@users.noreply.github.com>
2020-12-08 15:32:24 +0000

    Making Config properties adhere to Sink or source connectors

--- src/main/java/com/microsoft/azure/cosmosdb/kafka/connect/source/SourceSettings.java
+++ src/main/java/com/microsoft/azure/cosmosdb/kafka/connect/source/SourceSettings.java
@@ -1,7 +1,9 @@
 package com.microsoft.azure.cosmosdb.kafka.connect.source;
 
 import com.microsoft.azure.cosmosdb.kafka.connect.BooleanSetting;
+import com.microsoft.azure.cosmosdb.kafka.connect.NumericSetting;
 import com.microsoft.azure.cosmosdb.kafka.connect.Setting;
+import com.microsoft.azure.cosmosdb.kafka.connect.SettingDefaults;
 import com.microsoft.azure.cosmosdb.kafka.connect.Settings;
 import org.apache.commons.collections4.ListUtils;
 
@@ -15,13 +17,24 @@ public class SourceSettings extends Settings {
     private String postProcessor;
     private boolean startFromBeginning;
     private final List<Setting> sourceSettings = Arrays.asList(
-            new Setting(Settings.PREFIX + ".source.post-processor", "Comma-separated list of Source Post-Processor class names to use for post-processing",
+            // Add all source settings here:
+            new NumericSetting(PREFIX + ".task.timeout", "The max number of milliseconds the source task will use to read documents before sending them to Kafka.",
+                    "Task Timeout", SettingDefaults.TASK_TIMEOUT, this::setTaskTimeout, this::getTaskTimeout),
+            new NumericSetting(PREFIX + ".task.buffer.size", "The max size the container of documents the source task will buffer before sending them to Kafka.",
+                    "Task reader buffer size", SettingDefaults.TASK_BUFFER_SIZE, this::setTaskBufferSize, this::getTaskBufferSize),
+            new NumericSetting(PREFIX + ".task.batch.size","The max number of of documents the source task will buffer before sending them to Kafka.",
+                    "Task batch size", SettingDefaults.TASK_BATCH_SIZE, this::setTaskBatchSize, this::getTaskBatchSize),
+            new NumericSetting(PREFIX + ".task.poll.interval","The default polling interval in milliseconds that a source task polls for changes.",
+                    "Task poll interval", SettingDefaults.TASK_POLL_INTERVAL, this::setTaskPollInterval, this::getTaskPollInterval),
+            new Setting(Settings.PREFIX + ".containers", "A comma delimited list of source container names.",
+                    "Container Names List", this::setContainerList, this::getContainerList),
+            new Setting(Settings.PREFIX + ".source.post-processor", "Comma-separated list of Source Post-Processor class names to use for post-processing.",
                     "Source post-processor", this::setPostProcessor, this::getPostProcessor),
-            new Setting(Settings.PREFIX + ".assigned.container", "The CosmosDB Feed Container assigned to the task.",
+            new Setting(Settings.PREFIX + ".assigned.container", "The Cosmos DB Feed Container assigned to the task.",
                     "Assigned Container", this::setAssignedContainer, this::getAssignedContainer),
-            new Setting(Settings.PREFIX + ".worker.name", "The CosmosDB worker name.",
+            new Setting(Settings.PREFIX + ".worker.name", "The Cosmos DB worker name.",
                     "Worker name", this::setWorkerName, this::getWorkerName),
-            new BooleanSetting(Settings.PREFIX + ".changefeed.startFromBeginning", "If the change feed should start from beginning",
+            new BooleanSetting(Settings.PREFIX + ".changefeed.startFromBeginning", "Whether the change feed should start from beginning.",
                     "Change Feed start from beginning", SourceSettingDefaults.CHANGE_FEED_START_FROM_BEGINNING, this::setStartFromBeginning, this::isStartFromBeginning)
 
     );
