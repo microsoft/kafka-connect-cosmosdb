33828fa191f1c8d7793e168076fc69cdf67175a5
sivamu <67930841+sivamu@users.noreply.github.com>
2021-01-05 18:10:05 +0000

    Adding docs for using local dev containers, updating devcontainer setup
    Updating script call
    
    Apt update call change
    
    Using old docker debian script
    
    Docs for confluent platform setup in local devcontainers

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-# copy vscode files
-mkdir -p .vscode && cp docs/vscode-template/* .vscode
+# Workaround to get docker working as nonroot inside a devcontainer on a local machine
+# NOTE: The main docker-debian.sh script https://github.com/microsoft/vscode-dev-containers/blob/master/script-library/docker-debian.sh
+# did not work out of the box for setting up docker as nonroot
+sudo chmod 660 /var/run/docker.sock
+sudo chgrp docker /var/run/docker.sock
