f0b4fe877109f5dd38218909b79e555547f95edd
Jorge Machado <4804546+jomach@users.noreply.github.com>
2022-02-07 13:35:37 +0000

    Fixed tests

--- src/test/java/com/azure/cosmos/kafka/connect/sink/CosmosDBSinkTaskTestNotFails.java
+++ src/test/java/com/azure/cosmos/kafka/connect/sink/CosmosDBSinkTaskTestNotFails.java
@@ -26,7 +26,7 @@ import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.fail;
 import static org.mockito.Mockito.*;
 
-public class CosmosDBSinkTaskTest {
+public class CosmosDBSinkTaskTestNotFails {
     private final String topicName = "testtopic";
     private final String containerName = "container666";
     private final String databaseName = "fakeDatabase312";
@@ -42,6 +42,7 @@ public class CosmosDBSinkTaskTest {
         Map<String, String> settingAssignment = CosmosDBSinkConfigTest.setupConfigs();
         settingAssignment.put(CosmosDBSinkConfig.COSMOS_CONTAINER_TOPIC_MAP_CONF, topicName + "#" + containerName);
         settingAssignment.put(CosmosDBSinkConfig.COSMOS_DATABASE_NAME_CONF, databaseName);
+        settingAssignment.put(CosmosDBSinkConfig.TOLERANCE_ON_ERROR_CONFIG, "All");
         CosmosDBSinkConfig config = new CosmosDBSinkConfig(settingAssignment);
         FieldUtils.writeField(testTask, "config", config, true);
 
@@ -54,50 +55,17 @@ public class CosmosDBSinkTaskTest {
 
         FieldUtils.writeField(testTask, "client", mockCosmosClient, true);
     }
+    
 
     @Test
-    public void testPutPlainTextString() {
-        Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
-
-        SinkRecord record = new SinkRecord(topicName, 1, stringSchema, "nokey", stringSchema, "foo", 0L);
-        assertNotNull(record.value());
-
-        //Make mock connector to serialize a non-JSON payload
-        when(mockContainer.upsertItem(any())).then((invocation) -> {
-            Object item = invocation.getArgument(0);
-            //Will throw exception:
-            try {
-                JsonNode jsonNode = new ObjectMapper().readTree(item.toString());
-                assertNotNull(jsonNode);
-                return null;
-            } catch (JsonParseException jpe) {
-                throw new BadRequestException("Unable to serialize JSON request", jpe);
-            }
-        });
-
-        try {
-            testTask.put(Arrays.asList(record));
-            fail("Expected ConnectException on bad message");
-        } catch (ConnectException ce) {
-
-        } catch (Throwable t) {
-            fail("Expected ConnectException, but got: " + t.getClass().getName());
-        }
-
-        verify(mockContainer, times(1)).upsertItem("foo");
-    }
-
-    @Test
-    public void testPutMap() {
+    public void testPutMapThatFailsDoesNotStopTask() throws JsonProcessingException {
         Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
         Schema mapSchema = new ConnectSchema(Schema.Type.MAP);
-        Map<String, String> map = new HashMap<>();
-        map.put("foo", "baaarrrrrgh");
+        when(mockContainer.upsertItem(any())).thenThrow(new BadRequestException("Something"));
+        SinkRecord record = new SinkRecord(topicName, 1, stringSchema, "nokey", mapSchema, "{", 0L);
 
-        SinkRecord record = new SinkRecord(topicName, 1, stringSchema, "nokey", mapSchema, map, 0L);
-        assertNotNull(record.value());
         testTask.put(Arrays.asList(record));
-        verify(mockContainer, times(1)).upsertItem(map);
+
     }
 }
 
