3ddce08bf3ab0e5edc9f3ebc1b835756e5a5442c
Jorge Machado <4804546+jomach@users.noreply.github.com>
2022-02-08 10:26:57 +0000

    * added test that verifies for ErrantRecordReporter call

--- src/test/java/com/azure/cosmos/kafka/connect/sink/CosmosDBSinkTaskTestNotFails.java
+++ src/test/java/com/azure/cosmos/kafka/connect/sink/CosmosDBSinkTaskTestNotFails.java
@@ -13,13 +13,17 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.kafka.connect.data.ConnectSchema;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.sink.ErrantRecordReporter;
 import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTaskContext;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static junit.framework.TestCase.assertNotNull;
@@ -33,6 +37,8 @@ public class CosmosDBSinkTaskTestNotFails {
     private CosmosDBSinkTask testTask;
     private CosmosClient mockCosmosClient;
     private CosmosContainer mockContainer;
+    private SinkTaskContext mockContext = Mockito.mock(SinkTaskContext.class);
+    private ErrantRecordReporter mockErrantReporter = Mockito.mock(ErrantRecordReporter.class);
 
     @Before
     public void setup() throws IllegalAccessException {
@@ -42,7 +48,7 @@ public class CosmosDBSinkTaskTestNotFails {
         Map<String, String> settingAssignment = CosmosDBSinkConfigTest.setupConfigs();
         settingAssignment.put(CosmosDBSinkConfig.COSMOS_CONTAINER_TOPIC_MAP_CONF, topicName + "#" + containerName);
         settingAssignment.put(CosmosDBSinkConfig.COSMOS_DATABASE_NAME_CONF, databaseName);
-        settingAssignment.put(CosmosDBSinkConfig.TOLERANCE_ON_ERROR_CONFIG, "All");
+        settingAssignment.put(CosmosDBSinkConfig.TOLERANCE_ON_ERROR_CONFIG, "all");
         CosmosDBSinkConfig config = new CosmosDBSinkConfig(settingAssignment);
         FieldUtils.writeField(testTask, "config", config, true);
 
@@ -52,20 +58,35 @@ public class CosmosDBSinkTaskTestNotFails {
         when(mockCosmosClient.getDatabase(anyString())).thenReturn(mockDatabase);
         mockContainer = Mockito.mock(CosmosContainer.class);
         when(mockDatabase.getContainer(any())).thenReturn(mockContainer);
+        when(mockContext.errantRecordReporter()).thenReturn(mockErrantReporter);
 
         FieldUtils.writeField(testTask, "client", mockCosmosClient, true);
+
     }
-    
+    @After()
+    public void resetContext() throws IllegalAccessException {
+        FieldUtils.writeField(testTask,  "context", null, true);
+    }
+
 
     @Test
-    public void testPutMapThatFailsDoesNotStopTask() throws JsonProcessingException {
+    public void testPutMapThatFailsDoesNotStopTask() throws JsonProcessingException, IllegalAccessException {
+
         Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
         Schema mapSchema = new ConnectSchema(Schema.Type.MAP);
         when(mockContainer.upsertItem(any())).thenThrow(new BadRequestException("Something"));
         SinkRecord record = new SinkRecord(topicName, 1, stringSchema, "nokey", mapSchema, "{", 0L);
-
-        testTask.put(Arrays.asList(record));
-
+        testTask.put(List.of(record));
+    }
+    @Test
+    public void testPutMapThatFailsDoesNotStopTaskWithdlq() throws JsonProcessingException, IllegalAccessException {
+        FieldUtils.writeField(testTask,  "context", mockContext, true);
+        Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
+        Schema mapSchema = new ConnectSchema(Schema.Type.MAP);
+        when(mockContainer.upsertItem(any())).thenThrow(new BadRequestException("Something"));
+        SinkRecord record = new SinkRecord(topicName, 1, stringSchema, "nokey", mapSchema, "{", 0L);
+        testTask.put(List.of(record));
+        verify(mockContext.errantRecordReporter(), times(1)).report(any(), any());
     }
 }
 
