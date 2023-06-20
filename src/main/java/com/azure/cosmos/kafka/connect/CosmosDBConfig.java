1ab7933ec5a439fe4295d89854aa05da911dfb52
tvaron3 <70857381+tvaron3@users.noreply.github.com>
2023-06-20 11:16:56 -0700

    Apply suggestions from code review
    Fixed wording of flag in documentation and changelog
    
    Co-authored-by: Kushagra Thapar <kuthapar@microsoft.com>

--- src/main/java/com/azure/cosmos/kafka/connect/CosmosDBConfig.java
+++ src/main/java/com/azure/cosmos/kafka/connect/CosmosDBConfig.java
@@ -71,7 +71,7 @@ public class CosmosDBConfig extends AbstractConfig {
     private static final boolean DEFAULT_COSMOS_SINK_BULK_ENABLED = true;
 
     public static final String COSMOS_SINK_BULK_COMPRESSION_ENABLED_CONF = "connect.cosmos.sink.bulk.compression.enabled";
-    private static final String COSMOS_SINK_BULK_COMPRESSION_ENABLED_DOC = "Flag to indicate whether Cosmos DB in bulk mode will allow duplicates in the same batch to be written for Sink connector. By default it is true.";
+    private static final String COSMOS_SINK_BULK_COMPRESSION_ENABLED_DOC = "Flag to indicate whether Cosmos DB in bulk mode will compress and resolve duplicates in the same batch to be written for Sink connector. By default it is true. ";
     private static final boolean DEFAULT_COSMOS_SINK_BULK_COMPRESSION_ENABLED = true;
 
     public static final String COSMOS_SINK_MAX_RETRY_COUNT = "connect.cosmos.sink.maxRetryCount";
